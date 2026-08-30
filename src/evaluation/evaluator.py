3d963a52ec47510599c45261d435cd64d947ad5e
arya237 <naderiarya20@gmail.com>
2026-08-30 11:59:57 +0330

    fix evaluate

--- src/evaluation/evaluator.py
+++ src/evaluation/evaluator.py
@@ -58,9 +58,19 @@ class Evaluator:
                 dim=1,
             )
 
-            batch_predictions = batch_probabilities.argmax(
-                dim=1
+            batch_probabilities = torch.softmax(
+                logits,
+                dim=1,
             )
+
+            batch_predictions = torch.sum(
+                batch_probabilities * self.criterion.class_indices,
+                dim=1,
+            )
+
+            batch_predictions = torch.round(
+                batch_predictions
+            ).long()
 
             labels.extend(
                 batch_labels.cpu().tolist()
